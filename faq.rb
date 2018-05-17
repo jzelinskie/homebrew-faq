c3edc1ea2589b70ac04258bf0f839d8a0fa8117f
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2018-05-17 17:46:12 -0400

    remove HEAD option
    At least until I'm not lazy and add support for building from source.

--- faq.rb
+++ faq.rb
@@ -4,7 +4,6 @@ class Faq < Formula
   desc "Format Agnostic jQ"
   homepage "https://github.com/jzelinskie/faq"
   url "https://github.com/jzelinskie/faq/releases/download/0.0.1/faq-darwin-amd64"
-  head "https://github.com/jzelinskie/faq.git"
   sha256 "72bb04487c1a6c4e90f8a04f68add383b4dd293e9d29413a35f4611bfa18c6cb"
 
   depends_on "jq"
