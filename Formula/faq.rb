642ffb2ea58567c04a1d38b8bddf015259534d4a
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2019-02-14 20:35:53 -0500

    bump to 0.0.6

--- Formula/faq.rb
+++ Formula/faq.rb
@@ -3,9 +3,9 @@ require "formula"
 class Faq < Formula
   desc "Format Agnostic jQ"
   homepage "https://github.com/jzelinskie/faq"
-  url "https://github.com/jzelinskie/faq/releases/download/0.0.5/faq-darwin-amd64"
-  sha256 "810b5011ac4ecef7609e98d724f944f27e26647dbbd84b642d97ac444de39d26"
-  version "0.0.5"
+  url "https://github.com/jzelinskie/faq/releases/download/0.0.6/faq-darwin-amd64"
+  sha256 "bfcd6f527d1ba74db6bdd6bfb551a4db9c2c72f01baebf8069e9849b93dceef9"
+  version "0.0.6"
 
   depends_on "jq"
   bottle :unneeded
